5755d041d6ac86223935cea1979febe928c43c3b
neverorfrog <97flavio.maiorana@gmail.com>
2025-10-21 11:20:48 +0200

    fix

--- booster_gym/deploy/deploy.py
+++ booster_gym/deploy/deploy.py
@@ -217,7 +217,7 @@ def main():
     parser.add_argument("--config", required=True, type=str, help="Name of the configuration file.")
     parser.add_argument("--net", type=str, default="127.0.0.1", help="Network interface for SDK communication.")
     args = parser.parse_args()
-    cfg_file = os.path.join("configs", args.config)
+    cfg_file = os.path.join("booster_gym/deploy/configs", args.config)
     print(cfg_file)
 
     print(f"Starting custom controller, connecting to {args.net} ...")
